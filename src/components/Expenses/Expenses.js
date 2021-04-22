dc466bb0606d305ec48bc04816b59e723ed98947
xaviguasch <xaviguasch@gmail.com>
2021-04-22 18:48:37 +0200

    fitered components according to a date picker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,11 +13,20 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear)
   }
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items.map((expense) => (
-        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
       ))}
     </Card>
   )

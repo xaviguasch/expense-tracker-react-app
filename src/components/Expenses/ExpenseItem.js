afc0e63b5d49243b59306e59ccb6fcd386208f88
xaviguasch <xaviguasch@gmail.com>
2021-04-22 19:25:25 +0200

    added ExpenseList component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,16 @@ import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button>Change Title</button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+        <button>Change Title</button>
+      </Card>
+    </li>
   )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from './ExpenseItem'
-import ExpensesFilter from './ExpensesFilter'
 import React, { useState } from 'react'
 
+import ExpensesFilter from './ExpensesFilter'
 import Card from '../UI/Card'
+import ExpensesList from './ExpensesList'
 
 import './Expenses.css'
 
@@ -17,23 +17,10 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear
   })
 
-  let expensesContent = <p>No expenses found.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <Card className='expenses'>
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   )
 }
